0e98e14d39cffbcbebc6a21ad1f9f7b9584d4acb
Marcin Szopik and Rochelle Roberts <rochelle@>
2019-06-03 13:18:39 -0700

    added multiplay divide

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,25 @@ function add(number1,number2){
 	return number1 + number2;
 };
 
-var sum;
-//sum = add(10,5);
-//alert(sum);
+function subtract(number1,number2){
+  return number1 - number2;
+};
+
+function multiply(number1,number2){
+ return number1 * number2;
+};
+
+function divide(number1,number2){
+  return number1 / number2;
+}
+
+var results;
 
 var number1 = prompt("Enter a number:"); // result is a sting
 var numberInt1 = parseInt(number1); //convert to integer for adding
 
 var number2 = parseInt(prompt("Enter a second number:"));
-
-
-
-
-sum = add(numberInt1, number2);
-alert(sum);
+//results = subtract(numberInt1, number2);
+//results = add(numberInt1, number2);
+results = divide(number1,number2);
+alert(results);
